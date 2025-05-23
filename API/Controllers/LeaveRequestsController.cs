95299eece63d36f9019b7062ac93723534285322
Puria Nosrati <purya.nosraty@gmail.com>
2025-05-23 14:38:44 +0330

    2025-05-23

--- API/Controllers/LeaveRequestsController.cs
+++ API/Controllers/LeaveRequestsController.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Interfaces;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class LeaveRequestsController : ControllerBase
+public class LeaveRequestsController(
+    IEmployeeRepository employeeRepository,
+    ILeaveRequestService leaveRequestService,
+    ILeaveRequestRepository leaveRequestRepository
+    ) : ControllerBase
 {
+    private readonly ILeaveRequestService
+        _leaveRequestService = leaveRequestService;
+
+    private readonly IEmployeeRepository
+        _employeeRepository = employeeRepository;
+
+    private readonly ILeaveRequestRepository
+        _leaveRequestRepository = leaveRequestRepository;
+
+    [HttpGet]
+    public IActionResult GetList()
+    {
+        var x = _leaveRequestService.GetAllAsync();
+        var a = _leaveRequestRepository.GetAllAsync();
+        var b = _employeeRepository.GetAllAsync();
+
+        return Ok(x);
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using API.Middlewares;
 using Infrastructure.Data;
 using System.Globalization;
 using System.Threading.Tasks;
@@ -11,132 +12,134 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using API.Middlewares;
 
 namespace API;
 
 internal static class Program
 {
-	static Program()
-	{
-	}
+    static Program()
+    {
+    }
 
-	private static async Task Main()
-	{
-		var webApplication = new WebApplicationOptions
-		{
-			EnvironmentName = Environments.Development
-			//EnvironmentName = Environments.Production
-		};
+    private static async Task Main()
+    {
+        var webApplication = new WebApplicationOptions
+        {
+            EnvironmentName = Environments.Development
+            //EnvironmentName = Environments.Production
+        };
 
-		var builder = WebApplication.CreateBuilder(options: webApplication);
-
-
-		// Add services to the container.
-		builder.Services.AddControllers();
-
-		// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddOpenApi();
+        var builder = WebApplication.CreateBuilder(options: webApplication);
 
 
-		builder.Services.Configure
-			<RequestLocalizationOptions>(option =>
-			{
-				var supportedCultures = new[]
-				{
-				new CultureInfo(name: "fa-IR"),
-				new CultureInfo(name: "en-US"),
-			};
+        // Add services to the container.
+        builder.Services.AddControllers();
 
-				option.SupportedCultures = supportedCultures;
-				option.SupportedUICultures = supportedCultures;
-
-				option.DefaultRequestCulture =
-					new RequestCulture(culture: "en-US", uiCulture: "en-US");
-			});
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddOpenApi();
 
 
-		builder.Services.AddDbContext<AppDbContext>(option =>
-			option.UseSqlServer(builder.Configuration
-				.GetConnectionString(name: nameof(Domain.Shared.Utility.Const.DefaultConnection))
-				)
-			)
-		;
+        builder.Services.Configure
+            <RequestLocalizationOptions>(option =>
+            {
+                var supportedCultures = new[]
+                {
+                new CultureInfo(name: "fa-IR"),
+                new CultureInfo(name: "en-US"),
+            };
+
+                option.SupportedCultures = supportedCultures;
+                option.SupportedUICultures = supportedCultures;
+
+                option.DefaultRequestCulture =
+                    new RequestCulture(culture: "en-US", uiCulture: "en-US");
+            });
 
 
-		builder.Services
-			.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			})
-			.AddJwtBearer(options =>
-			{
-				options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidIssuer = builder.Configuration["Jwt:Issuer"],
-
-					ValidateAudience = true,
-					ValidAudience = builder.Configuration["Jwt:Audience"],
-
-					ValidateIssuerSigningKey = true,
-
-					IssuerSigningKey = new SymmetricSecurityKey(
-						Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!)
-					),
-
-					ValidateLifetime = true,
-
-					ClockSkew = TimeSpan.Zero
-				};
-
-				options.Events = new JwtBearerEvents
-				{
-					OnAuthenticationFailed = context =>
-					{
-						return Task.CompletedTask;
-					}
-				};
-			})
-		;
+        builder.Services.AddDbContext<AppDbContext>(option =>
+            option.UseSqlServer(builder.Configuration
+                .GetConnectionString(name: nameof(Domain.Shared.Utility.Const.DefaultConnection))
+                )
+            )
+        ;
 
 
-		var app = builder.Build();
+        builder.Services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!)
+                    ),
+
+                    ValidateLifetime = true,
+
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        return Task.CompletedTask;
+                    }
+                };
+            })
+        ;
 
 
-		//using (var scope = app.Services.CreateScope())
-		//{
-		//	var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-		//	await appDbContext.Database.MigrateAsync();
-		//}
-
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseDeveloperExceptionPage();
-			app.MapOpenApi();
-		}
-		else
-		{
-			//app.UseExceptionHandler("/Errors/Error");
-			app.UseHsts();
-		}
+        var app = builder.Build();
 
 
-		app.UseHttpsRedirection();
-		app.UseStaticFiles();
-		app.UseRouting();
+        //using (var scope = app.Services.CreateScope())
+        //{
+        //	var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        //	await appDbContext.Database.MigrateAsync();
+        //}
 
-		app.UseAuthentication();
-		app.UseAuthorization();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapOpenApi();
+        }
+        else
+        {
+            //app.UseExceptionHandler("/Errors/Error");
+            app.UseHsts();
+        }
 
-		app.UseCultureCookie();
-		app.UseGlobalException();
 
-		app.MapControllers();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseCultureCookie();
+        app.UseGlobalException();
+
+        app.MapControllers();
 
 
-		await app.RunAsync();
-	}
+        await app.RunAsync();
+    }
 }
